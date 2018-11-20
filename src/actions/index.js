be07a2bd798b269f1fe1c11de0bc128f56a3085d
SuperLucyFight <454310349@qq.com>
2018-11-20 17:19:31 +0800

    add: react-redux async

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,52 @@
-let nextTodoId = 0
-export const addTodo = text => ({
-  type: 'ADD_TODO',
-  id: nextTodoId++,
-  text
+export const REQUEST_POSTS = 'REQUEST_POSTS'
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
+export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
+
+export const selectSubreddit = subreddit => ({
+  type: SELECT_SUBREDDIT,
+  subreddit
 })
-export const setVisibilityFilter = filter => (
-  {
-    type: 'SET_VISIBILITY_FILTER',
-    filter
+
+export const invalidateSubreddit = subreddit => ({
+  type: INVALIDATE_SUBREDDIT,
+  subreddit
+})
+
+export const requestPosts = subreddit => ({
+  type: REQUEST_POSTS,
+  subreddit
+})
+
+export const receivePosts = (subreddit, json) => ({
+  type: RECEIVE_POSTS,
+  subreddit,
+  // posts: json.data.children.map(child => child.data),
+  posts: json,
+  receivedAt: Date.now()
+})
+
+const fetchPosts = subreddit => dispatch => {
+  dispatch(requestPosts(subreddit))
+  return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+    .then(response => response.json(),
+      error => [{title: 'react', id: 1}, {title: 'react is ',id: 2}])
+    .then(json => dispatch(receivePosts(subreddit, json)))
+}
+
+const shouldFetchPosts = (state, subreddit) => {
+  const posts = state.postsBySubreddit[subreddit]
+  if (!posts) {
+    return true
   }
-)
-export const toggleTodo = id => (
-  {
-    type: 'TOGGLE_TODO',
-    id
+  if (posts.isFetching) {
+    return false
   }
-)
-// export const VisibilityFilters = {
-//   SHOW_ALL: 'SHOW_ALL',
-//   SHOW_COMPLETED: 'SHOW_COMPLETED',
-//   SHOW_ACTIVE: 'SHOW_ACTIVE'
-// }
+  return posts.didInvalidate
+}
+
+export const fetchPostsIfNeeded = subreddit => (dispatch, getState) => {
+  if (shouldFetchPosts(getState(), subreddit)) {
+    return dispatch(fetchPosts(subreddit))
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import reducer from './reducers'
+import App from './containers/App'
 
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import todoApp from './reducers/index'
+const middleware = [ thunk ]
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
 
-const store = createStore(todoApp)
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+)
 
-ReactDOM.render(
+render(
   <Provider store={store}>
-    <App/>
-  </Provider>
-  , document.getElementById('root'));
-registerServiceWorker();
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
